0ed279a9b63ccbe580119e58b82ecd0608a6377b
GreamFRz <94147946+igream@users.noreply.github.com>
2024-11-10 23:37:55 -0600

    Add files via upload

---
+++ Rutas.py
@@ -0,0 +1,12 @@
+import os
+from pathlib import Path
+
+# Crear carpeta
+os.makedirs('nueva_carpeta', exist_ok=True)
+
+# Verificar si una ruta existe
+ruta = Path('archivo.txt')
+print(ruta.exists())
+
+# Obtener nombre del archivo y extensión
+print(ruta.stem, ruta.suffix)
